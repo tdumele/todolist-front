bad039f744f5ba92d95f3cb880d1940597e9ca9d
Teddy Dumélé <teddy.dumele@gmail.com>
2024-12-12 23:12:00 +0100

    :bug: adding default page, before selecting todolist

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+
+  return (
+    <div className="flex-1 p-4">
+      <h1 className="text-3xl font-semibold mb-10">Good morning, Teddy! 👋</h1>
+      <h2>Please, select one of your todolist</h2>
+    </div>
+  );
+}
